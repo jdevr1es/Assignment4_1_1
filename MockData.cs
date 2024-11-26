2a7242b3ccb846b733fc0e0cd6c80801a4270c8c
John DeVries <johndevries49@gmail.com>
2024-11-26 07:18:29 -0800

    Add project files.

---
+++ MockData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4_1_1
+{
+    internal class MockData
+    {
+
+
+
+        // Populate the collections with mock data
+        public static Dictionary<string,Person> CreateData()
+        {
+            Dictionary<string, Person> Persons = new Dictionary<string, Person>();
+
+            Person person1 = new Person() { FirstName = "Kevin", LastName = "Liu", MobilePhone = "916-555-1234", WorkPhone = "916-555-4567", Address = "Sequim WA" };
+            Persons.Add(person1.FullName, person1);
+
+            Person person2 = new Person() { FirstName = "James", LastName = "Smith", MobilePhone = "916-555-7890", WorkPhone = "916-555-6543", Address = "Seattle WA" };
+            Persons.Add(person2.FullName, person2);
+
+
+
+            return Persons;
+
+        }
+    }
+}
+
---
+++ Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4_1_1
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MobilePhone { get; set; }
+        public string WorkPhone { get; set; }
+        public string Address { get; set; }
+
+        private string fullName;
+        public string FullName
+        {
+            get
+            {
+                return LastName + FirstName;
+            }
+
+
+        }
+    }
+}
